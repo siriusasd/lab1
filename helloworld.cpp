48efbeab600d94c152866aceee5f62f6fd745376
siriusasd <shawn1.2311@gmail.com>
2021-03-22 05:57:54 -0700

    add helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int main(void) {
-    for(int i=1; i<10; ++i) {
+int main (void) {
+    // print hello world for 9 times
+    for (int i=1; i<10; ++i) {
         cout << "hello world " << i << " times!\n";
     }
     return 0;
